c41be4cecabdd7de7a9e68c3c38d1577a9192115
ClaytonDewey <clay@dryadmedia.com>
2025-12-05 09:04:15 -0600

    create: Set up & style footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <p className='attribution'>
+        Challenge by{' '}
+        <a
+          href='https://www.frontendmentor.io?ref=challenge'
+          target='_blank'
+          rel='noreferrer'>
+          Frontend Mentor
+        </a>
+        .<br /> Coded by{' '}
+        <a href='https://www.claytondewey.com' target='_blank' rel='noreferrer'>
+          Clayton Dewey
+        </a>
+        .
+      </p>
+    </footer>
+  );
+};
+export default Footer;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as FileUpload } from './FileUpload';
+export { default as Footer } from './Footer';
 export { default as Form } from './Form';
 export { default as Header } from './Header';
 export { default as Input } from './Input';
